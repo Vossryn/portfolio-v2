fc869fae56229606a357ea2b34beddf9aabb58e2
Britton Flynt <vossryn@gmail.com>
2024-06-11 18:50:44 -0400

    checking in changes

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import "@/css/glitch-effect.css";
 import { useEffect, useState } from "react";
 
-const titles = ["Front-end", "Back-end", "UX/UI"];
+const titles = ["Front-end", "Back-end"];
 
 export default function Hero() {
   const [index, setIndex] = useState(0);
@@ -23,22 +23,22 @@ export default function Hero() {
   return (
     <div className="flex-none h-[calc(100%-60px)] flex flex-col justify-center items-center bg-hero-pattern bg-cover bg-top">
       <div className="grow w-full flex flex-col items-center justify-center overflow-hidden">
-        <div className="flex-none relative m-8">
+        <div className="flex-none relative mx-4 mt-4 md:mx-8 md:mt-8">
           <Image
             src="/android-chrome-512x512.png"
             alt="Site Logo Image"
-            className="w-full"
+            className="w-full h-full"
             width={500}
             height={500}
           />
-          <div className="absolute top-8 lg:top-16 lg:-left-4 -rotate-12 text-center text-7xl lg:text-9xl font-cyberpunk text-yellow-300">
+          <div className="absolute top-8 lg:top-16 lg:-left-4 -rotate-12 text-center text-6xl lg:text-9xl font-cyberpunk text-yellow-300">
             Britton
             <br />
             Flynt
           </div>
         </div>
         <div
-          className="absolute bottom-0 w-full hero glitch layers font-orbitron text-center text-4xl"
+          className="h-fit hero glitch layers font-orbitron text-center text-3xl lg:text-5xl"
           data-text={titles[index]}
         >
           <span>{titles[index]}</span>
