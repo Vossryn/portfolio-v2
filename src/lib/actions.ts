ad03508e2a4938c160ae65586c93831fc6ab2eb7
Britton Flynt <vossryn@gmail.com>
2024-09-25 11:46:34 -0400

    Tweaking email address

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -14,7 +14,7 @@ export async function sendEmail({
   const resend = new Resend(process.env.RESEND_API_KEY);
 
   const { data, error } = await resend.emails.send({
-    from: email,
+    from: "Vossryn Portfolio <vossrynportfolio@resend.dev>",
     to: ["vossryn@gmail.com"],
     subject: "Contact Request from Portfolio",
     react: EmailTemplate({ email, body }),
